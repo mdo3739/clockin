6d7fa035e92762407a4fbf201b4b611d95760530
Michael Do <mdo3739@gmail.com>
2016-04-01 04:21:51 +0000

    switched to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331182459) do
+ActiveRecord::Schema.define(version: 20160331183954) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "employees", ["user_id"], name: "index_employees_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
